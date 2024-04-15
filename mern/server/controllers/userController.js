3eddd1557936af97cd676ae7e9691570650bb5fe
Granicus0 <vjos612@aucklanduni.ac.nz>
2024-04-16 01:38:13 +1200

    Added auth context, finished user login and signup API's

--- mern/server/controllers/userController.js
+++ mern/server/controllers/userController.js
@@ -1,23 +1,54 @@
 import User from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
 
+// Function to create a JWT using the JWT library
+const createToken = (_id) => {
+    // JWT takes in the ID of the user, secret encoding string, and validity period
+    return jwt.sign({ _id: _id }, 'TEST_ENCODING_STRING', { expiresIn: '3d' })
+}
 
 // User login
 export const loginUser = async (req, res) => {
-    res.json({ msg: 'login user' })
+
+    // Grab the users email and password from the request body
+    const { email, password } = req.body
+
+    try {
+
+        // Log in the user with their email and password
+        const user = await User.login(email, password)
+
+        // Create the users JWT
+        const token = createToken(user._id)
+
+        // Send back response with email and JWT
+        res.status(200).json({ email, token })
+
+    } catch (error) {
+
+        res.status(400).json({ error: error.message })
+
+    }
 };
 
 // User signup
 export const signupUser = async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
     try {
 
+        // Create a user with email and password
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+
+        // Create the users JWT
+        const token = createToken(user._id)
+
+        // Send back response with email and JWT
+        res.status(200).json({ email, token })
 
     } catch (error) {
 
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message })
 
     }
 
@@ -25,7 +56,7 @@ export const signupUser = async (req, res) => {
 
 // Test the API
 export const testUser = async (req, res) => {
-    res.json({msg: 'success'})
+    res.json({ msg: 'success' })
 };
 
 
--- mern/server/models/userModel.js
+++ mern/server/models/userModel.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
         unique: true
     },
     password: {
-        type: String, 
+        type: String,
         required: true
     }
 })
@@ -20,21 +20,21 @@ const userSchema = new Schema({
 userSchema.statics.signup = async function (email, password) {
 
     // Validate email and password
-    if(!email || !password) {
+    if (!email || !password) {
         throw Error('You must have both an email and password')
     }
 
     // User the validator library to check if an email is a valid email, and if the user chosen password is strong enough
-    if(!validator.isEmail(email)) {
+    if (!validator.isEmail(email)) {
         throw Error('Email is not valid')
     }
-    if(!validator.isStrongPassword(password)) {
+    if (!validator.isStrongPassword(password)) {
         throw Error('Choose a stronger password')
     }
 
     // Check if the email is already in use
-    const exists = await this.findOne({email});
-    if(exists) {
+    const exists = await this.findOne({ email });
+    if (exists) {
         throw Error('Email already exists')
     }
 
@@ -43,9 +43,35 @@ userSchema.statics.signup = async function (email, password) {
     // Use the bcrypt library to hash the actual password, adding the salt
     const finalHashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await this.create({email, password: finalHashedPassword})
+    // Creates a user on MongoDB containing their email and salted hashed password
+    const user = await this.create({ email, password: finalHashedPassword })
 
     return user
+}
+
+
+// Logs in a user
+userSchema.statics.login = async function (email, password) {
+    // Validate email and password
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    // Check if the email is already in use
+    const user = await this.findOne({ email });
+    if (!user) {
+        throw Error('Invalid login details')
+    }
+
+    // Compare the passwords
+    const match = await bcrypt.compare(password, user.password);
+
+    if(!match) {
+        throw Error('Invalid login details')
+    }
+
+    return user;
+
 }
 
 const User = mongoose.model('User', userSchema);
