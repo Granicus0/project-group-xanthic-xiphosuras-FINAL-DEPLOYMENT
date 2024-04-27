d6cafe37f406a5beb1dc498a619d093ad18eec23
Granicus0 <vjos612@aucklanduni.ac.nz>
2024-04-28 01:34:25 +1200

    Added simple logout button

--- mern/client/src/pages/user_page/UserPage.jsx
+++ mern/client/src/pages/user_page/UserPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ModelCard from '../../components/ModelCard';
 import ModelCardv2 from '../../components/ModelCardv2';
+import {useLogout} from '../../hooks/useLogout'
+import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import "./UserPage.css";
 
@@ -8,6 +10,8 @@ function UserPage() {
   const [models, setModels] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { logout } = useLogout();
+  const navigate = useNavigate()
 
   const userDat = localStorage.getItem('user')
   const json = JSON.parse(userDat);
@@ -42,10 +46,17 @@ function UserPage() {
     return <p>Error: {error}</p>;
   }
 
+  const handleLogout = () => {
+    logout()
+    navigate('/')
+  }
+
+
   return (
     <div className='user-page-container'>
       <h1 className='user-page-header'> Hello, {userName}! </h1>
       <br></br>
+      <button onClick={handleLogout}> Logout </button>
       <h4 className='user-page-sub-header'> Your Current Models </h4>
       <div className="model-card-container2">
         {models && models.map((model) => (
