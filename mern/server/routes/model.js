4876ce81cf433af0ab4e5616fd699c4a84e5150f
Granicus0 <vjos612@aucklanduni.ac.nz>
2024-04-29 04:47:38 +1200

    Added comment on file uploading

--- mern/server/routes/model.js
+++ mern/server/routes/model.js
@@ -10,8 +10,8 @@ export default (io) => {
 
     // Route to begin training. This 'multer' thing just specifies that any files that the user uploads will be be stored
     // in the 'uploads/' path. Our relative path is the server/ folder, so user uploaded files will be in 'server/uploads'
+    // The file will be uploaded BEFORE 'beginModelTraining()' is called, so we can guarantee access inside the API route
     const upload = multer({ dest: 'uploads/' });
-
     router.post('/createModel', upload.single('csvFile'), (req, res) => {
         // Our POST request API to create a model will call the beginModelTraining API which will take in that socket IO object so it can communicate with the
         // front end in real time.
