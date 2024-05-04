18788362d593bcdc3e86bd01634995f478df1c8f
leihanZzZ <st18214@ormiston.school.nz>
2024-05-04 20:36:10 +1200

    add comments on hooks
    commenting

--- mern/client/src/hooks/useAuthContext.jsx
+++ mern/client/src/hooks/useAuthContext.jsx
@@ -1,10 +1,19 @@
+// Import the AuthContext to access the authentication state
 import { AuthContext } from "../context/AuthContext"
+// Import useContext from React to enable using a context value within a component or hook
 import { useContext } from "react"
 
+// Define a custom hook called `useAuthContext` for accessing the AuthContext
 export const useAuthContext = () => {
-
+  // Use the useContext hook to get the current context value from AuthContext
   const context = useContext(AuthContext)
-  if(!context) throw Error('useAuthContext must be used inside an AuthContextProvider')
 
+  // Check if the context is not available, which would happen if this hook is used outside of a provider
+  if(!context) {
+    // If context is not available, throw an error to inform the developer of incorrect usage
+    throw Error('useAuthContext must be used inside an AuthContextProvider')
+  }
+
+  // Return the context which includes authentication state and dispatch function
   return context
 }
