0546efe4648574f2ba053f16cc60923fdb43be2c
Alina <xxu599@aucklanduni.ac.nz>
2024-05-05 19:37:20 +1200

    change the modelcard and v2 style

--- mern/client/src/components/ModelCardv2.jsx
+++ mern/client/src/components/ModelCardv2.jsx
@@ -40,7 +40,9 @@ function ModelCardv2({modelInfo}) {
       </Link>
       <main className="main-content">
         <h1><a>{modelInfo.model_name}</a></h1>
-        <div className="flex-row">
+        
+      </main>
+      <div className="flex-row">
           <div className="coin-base">
             <h2>{modelInfo.model_type}</h2>
           </div>
@@ -48,7 +50,6 @@ function ModelCardv2({modelInfo}) {
             {isLoading ? 'Removing...' : 'Remove'}
           </button>
         </div>
-      </main>
     </div>
   );
 }
