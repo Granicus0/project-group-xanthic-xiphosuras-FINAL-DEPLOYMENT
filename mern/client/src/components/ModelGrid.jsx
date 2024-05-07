f39dae0a6d252d1c11db3b0aff0fded4e28502a9
Granicus0 <vjos612@aucklanduni.ac.nz>
2024-05-08 02:00:56 +1200

    Removed redundant method in modelgrid

--- mern/client/src/components/ModelGrid.jsx
+++ mern/client/src/components/ModelGrid.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ModelCardv2 from './ModelCardv2';
 import './css/Grid.css';
-import { useState } from 'react';
+
 function ModelGrid({ modelData, modelType }) {
-  
-
-
 
   const filteredModelData = modelType ? modelData.filter(info => info.model_type === modelType) : modelData;
-
-
-  const handleModelRemove = (modelId) => {
-    // Update the state to remove the model
-    setModelData(currentData => currentData.filter(model => model._id !== modelId));
-  };
 
   return (
     <div className="grid-container-component" data-testid="grid-container-component-test">
